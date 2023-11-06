c4d2b3717ca8cb1b4066e024810484fca30cc80a
Jacob <jacobyidev@gmail.com>
2023-11-06 06:49:46 -0800

    feat: add landing page and styling

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner(0);
+
+  // Initiate the wowjs
+  new WOW().init();
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500, "easeInOutExpo");
+    return false;
+  });
+})(jQuery);
